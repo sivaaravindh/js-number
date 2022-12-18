1dbb2cb0c37a0d8802656b5f1d870480eb6cdcfe
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-18 07:34:24 -0800

    index

---
+++ main.js
@@ -0,0 +1,18 @@
+let num=parseInt (prompt("Enter the value"));
+let add= 0;
+
+for( let i=1; i<=num; i++){
+    add += i;
+}
+
+document.write("The sum of natural numbers" + add)
+
+/*var a=parseInt (prompt ("enter the value"));
+			var b=parseInt (prompt ("enter the value"));
+			var c=0;
+			for(i=1;i<=a;i++)
+			{
+				c=c+b
+				
+			}
+			console.log(c)*/
